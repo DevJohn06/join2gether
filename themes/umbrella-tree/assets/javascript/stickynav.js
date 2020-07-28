eee23760cdd700bcf218ad88ee1c8da444d643a0
John Torralba <wet.johnt@gmail.com>
2020-07-28 13:30:46 -0400

    finish homepage

--- themes/umbrella-tree/assets/javascript/stickynav.js
+++ themes/umbrella-tree/assets/javascript/stickynav.js
@@ -1,3 +1,26 @@
+// (function($) {
+//     "use strict";
+
+//     var $navbar = $("#navbar"),
+//         y_pos = $navbar.offset().top,
+//         height = $navbar.height();
+
+//     $(document).scroll(function() {
+//         var scrollTop = $(this).scrollTop();
+
+//         if (scrollTop > y_pos + height) {
+//             $navbar.addClass("navbar-fixed").animate({
+//                 top: 0
+//             });
+//         } else if (scrollTop <= y_pos) {
+//             $navbar.removeClass("navbar-fixed").clearQueue().animate({
+//                 top: "0px"
+//             }, 0);
+//         }
+//     });
+
+// })(jQuery, undefined);
+
 (function($) {
     "use strict";
 
@@ -14,9 +37,29 @@
             });
         } else if (scrollTop <= y_pos) {
             $navbar.removeClass("navbar-fixed").clearQueue().animate({
-                top: "-48px"
+                top: "0px"
             }, 0);
         }
-    });
 
+        if ($navbar.hasClass("navbar-fixed") || $navbar.hasClass("navbar-secondary")){
+            $('#homeLogo').attr("src",getRootWebSitePath()+"/storage/app/media/logo.png");
+        } 
+    
+        else {
+            $('#homeLogo').attr("src",getRootWebSitePath()+"/storage/app/media/logo-w.png");
+        }
+    });
 })(jQuery, undefined);
+
+
+// GET ROOT PATH
+function getRootWebSitePath()
+{
+    var _location = document.location.toString();
+    var applicationNameIndex = _location.indexOf('/', _location.indexOf('://') + 3);
+    var applicationName = _location.substring(0, applicationNameIndex) + '/';
+    var webFolderIndex = _location.indexOf('/', _location.indexOf(applicationName) + applicationName.length);
+    var webFolderFullPath = _location.substring(0, webFolderIndex);
+
+    return webFolderFullPath;
+}
